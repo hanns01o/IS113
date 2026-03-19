cab11fb26cf664fb2a939494082723fb0b74843e
SuMyatMyatHtay <99980566+SuMyatMyatHtay@users.noreply.github.com>
2026-03-19 10:45:11 +0800

    home page top 3 recent shows modified

--- Project/controllers/homeController.js
+++ Project/controllers/homeController.js
@@ -3,7 +3,7 @@ const Watchlist = require("../models/Watchlist");
 exports.getHomePage = async (req, res) => {
   try {
     let watchlistMovies = [];
-    let featuredMovie = null;
+    let featuredMovies = [];
 
     if (req.session.userId) {
       const watchlistItems = await Watchlist.find({ userId: req.session.userId })
@@ -12,35 +12,35 @@ exports.getHomePage = async (req, res) => {
       watchlistMovies = watchlistItems
         .map(item => item.movieId)
         .filter(movie => movie);
-
-      if (watchlistMovies.length > 0) {
-        featuredMovie = watchlistMovies[0];
-      }
     }
 
-    const category = "popular"; 
+    const category = "popular";
     const response = await fetch(
-          `https://api.themoviedb.org/3/movie/${category}?api_key=${process.env.API_KEY}`
+      `https://api.themoviedb.org/3/movie/${category}?api_key=${process.env.API_KEY}`
     );
 
     const data = await response.json();
 
     if (data.results) {
-        featuredMovies = data.results.slice(0, 3).map(movie => ({
-            id: movie.id,
-            title: movie.title,
-            overview: movie.overview,
-            backdropUrl: `https://image.tmdb.org/t/p/original${movie.backdrop_path}`,
-            posterUrl: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-            rating: movie.vote_average,
-            releaseDate: movie.release_date
+      featuredMovies = data.results
+        .filter(movie => movie.backdrop_path)
+        .slice(0, 3)
+        .map(movie => ({
+          id: movie.id,
+          title: movie.title,
+          overview: movie.overview,
+          backdropUrl: `https://image.tmdb.org/t/p/original${movie.backdrop_path}`,
+          posterUrl: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+          rating: movie.vote_average,
+          releaseDate: movie.release_date
         }));
     }
 
+    const previewWatchlist = watchlistMovies.slice(0, 5);
 
     res.render("home", {
-      featuredMovie,
-      watchlistMovies
+      featuredMovies,
+      watchlistMovies: previewWatchlist
     });
   } catch (err) {
     console.error(err);
