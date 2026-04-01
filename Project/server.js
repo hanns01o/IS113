c6a34d7edda204bb3ad70ea3acdfdac83a8b9ba8
hanns01o <danishmn.2022@business.smu.edu.sg>
2026-04-01 11:19:33 +0800

    fixed search preview and report function for user sub movies

--- Project/server.js
+++ Project/server.js
@@ -92,7 +92,7 @@ app.get("/", (req, res) => {
 });
 
 const hostname = "127.0.0.1";
-const port = 8000;
+const port = 8080;
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
